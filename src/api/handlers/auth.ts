9dbf3350bca83549a6e21641041f7dc67be9fbff
reuthaselkorn <reut.haselkorn@gmail.com>
2023-12-31 23:17:25 +0200

    cosmetics

--- src/api/handlers/auth.ts
+++ src/api/handlers/auth.ts
@@ -2,78 +2,78 @@ import UserModel from "../../entities/user"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 
-    const login = async(req, res, next) => {
-        console.log('login')
-        const email = req.body.email
-        const password = req.body.password
+const register = async(req, res, next) => {
+    console.log('register')
+    const email = req.body.email
+    const password = req.body.password
 
-        if (email == null || password == null) {
-            return sendError(res, 'one of the fields is missing')
-        }
-
-        try {
-            const user = await UserModel.findOne({'email':email})
-
-            if (user == null) return sendError(res, 'user not found')
-
-            const match = await bcrypt.compare(password, user.password)
-            if (!match) return sendError(res, 'password is wrong')
-
-            const accessToken = await jwt.sign(
-                {"_id":user._id},
-                process.env.ACCESS_TOKEM_SECRET,
-                {expiresIn: process.env.JWT_TOKEN_EXPIRATION}
-            )
-
-            res.status(200).send({'access token':accessToken})
-        } catch(err) {
-            return sendError(res, 'failed login')
-        }
+    if (email == null || password == null) {
+        return sendError(res)
     }
+    
+    try {
+        const exists = await UserModel.exists({'email' : email})
 
-    const register = async(req, res, next) => {
-        console.log('register')
-        const email = req.body.email
-        const password = req.body.password
-
-        if (email == null || password == null) {
-            return sendError(res)
+        if (exists) {
+            return sendError(res, 'user already registered')
         }
         
-        try {
-            const exists = await UserModel.exists({'email' : email})
+        const salt = await bcrypt.genSalt(10)
+        const encryptedPassword = await bcrypt.hash(password, salt)
 
-            if (exists) {
-                return sendError(res, 'user already registered')
-            }
-            
-            const salt = await bcrypt.genSalt(10)
-            const encryptedPassword = await bcrypt.hash(password, salt)
+        const user = new UserModel({
+            'email':email,
+            'password':encryptedPassword
+        })
+        const newUser = await user.save()
+        res.status(200).send(newUser)
+    } catch (err) {
+        return sendError(res, (err instanceof Error) ? 'failed login: ' + err.message : 'failed registration')
+    }        
+}
 
-            const user = new UserModel({
-                'email':email,
-                'password':encryptedPassword
-            })
-            const newUser = await user.save()
-            res.status(200).send(newUser)
-        } catch (err) {
-            return sendError(res, 'failed registration')
-        }        
+const login = async(req, res, next) => {
+    console.log('login')
+    const email = req.body.email
+    const password = req.body.password
+
+    if (email == null || password == null) {
+        return sendError(res, 'one of the fields is missing')
     }
 
-    const sendError = (res, message?) => {
-        return res.status(400).send({
-            'status' : 'fail',
-            'message' : message == null ? 'not implemented' : message
-        })   
-    }
+    try {
+        const user = await UserModel.findOne({'email':email})
 
-    const logout = async(req, res, next) => {
-        console.log('logout')
-        res.status(400).send({
-            'status' : 'fail',
-            'message' : 'not implemented'
-        })   
-    }
+        if (user == null) return sendError(res, 'user not found')
 
-    export { register, login, logout };
+        const match = await bcrypt.compare(password, user.password)
+        if (!match) return sendError(res, 'password is wrong')
+
+        const accessToken = await jwt.sign(
+            {"_id":user._id},
+            process.env.ACCESS_TOKEM_SECRET,
+            {expiresIn: process.env.JWT_TOKEN_EXPIRATION}
+        )
+
+        res.status(200).send({'access token':accessToken})
+    } catch(err) {
+        return sendError(res, (err instanceof Error) ? 'failed login: ' + err.message : 'failed login')
+    }
+}
+
+const sendError = (res, message?) => {
+    return res.status(400).send({
+        'status' : 'fail',
+        'message' : message == null ? 'not implemented' : message
+    })   
+}
+
+const logout = async(req, res, next) => {
+    console.log('logout')
+    res.status(400).send({
+        'status' : 'fail',
+        'message' : 'not implemented'
+    })   
+}
+
+export { register, login, logout };
