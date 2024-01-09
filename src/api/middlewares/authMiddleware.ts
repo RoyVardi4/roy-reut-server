523605e7215431e2f095b2865d046e569b7c71bc
reuthaselkorn <reut.haselkorn@gmail.com>
2024-01-09 19:24:30 +0200

    fixed authentication and added middleware to all requests

--- src/api/middlewares/authMiddleware.ts
+++ src/api/middlewares/authMiddleware.ts
@@ -1,32 +1,23 @@
 import jwt from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
-import { refreshToken } from "../handlers/auth";
 
 const authenticate = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  console.log('auth middleware')
   const accessToken = req.headers.authorization;
-  const refreshTokenr = req.headers.refreshToken;
 
-  if (!accessToken && !refreshTokenr)
-    return res.sendStatus(401).send("Access Denied. No token provided.");
+  if (!accessToken)
+    return res.status(401).send("Access Denied. No token provided.");
 
   try {
     const user = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
     req["user"] = user;
     next();
   } catch (error) {
-    if (!refreshTokenr) {
-      return res.status(401).send("Access Denied. No refresh token provided.");
-    }
-
-    try {
-      refreshToken(req, res, next)
-    } catch (error) {
-      return res.status(400).send("Invalid Token.");
-    }
+    return res.status(401).send("Access Denied. Invalid token");
   }
 };
 
