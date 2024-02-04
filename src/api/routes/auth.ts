b4b61351f0c1b3278d404e6fa53f3cefbf050c23
RoyVardi4 <vardiroy4@gmail.com>
2024-02-04 21:58:27 +0200

    first route in swagger

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -5,13 +5,25 @@ import { authenticate } from "../middlewares/authMiddleware";
 const route = Router();
 
 export default (app: Router) => {
+  
   app.use("/auth", route);
 
-  route.post('/register', register)
+  route.post("/register", register);
 
-  route.post('/login', login)
+  route.post("/login", login);
 
-  route.post('/logout', authenticate, logout)
+  route.post("/logout", authenticate, logout);
 
-  route.get('/refreshToken', refreshToken)
+  /**
+ * @swagger
+ * /refreshToken:
+ *   get:
+ *     summary: Returns a new access token
+ *     responses:
+ *       '200':
+ *         description: access permited
+ *       '401':
+ *         description: access denied
+ */
+  route.get("/refreshToken", refreshToken);
 };
