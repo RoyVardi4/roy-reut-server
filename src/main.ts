e90eb30d5c4a0e4ef8831ea1c747cbc4919976cc
RoyVardi4 <vardiroy4@gmail.com>
2024-02-28 21:27:02 +0200

    first step

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,17 @@
-import express from "express";
+import express, { Application } from "express";
 import config from "./config/index";
 import Logger from "./lib/logger";
-import * as libs from "./lib";
-import mongoose from "mongoose";
+import appPromise from "./app";
 
-const app: express.Application = express();
-libs.default({ serverApp: app });
-mongoose
-  .connect(process.env.DATABASE_URL, {
-    dbName: process.env.DATABASE_NAME || "roy-reut-db",
-  })
-  .then(
-    () =>
-      app &&
-      app.listen(config.port, () => {
-        Logger.info(`
-⚡️---------------------------------------------------⚡️
-⚡️|	Running Node Server for ${process.env.NODE_ENV}	|⚡️
-⚡️|	    Ready now on port: ${config.port}           |⚡️
-⚡️---------------------------------------------------⚡️
-
+appPromise &&
+  appPromise.then((app) => {
+    const myApp = app as Application;
+    myApp.listen(config.port, () => {
+      Logger.info(`
+  ⚡️---------------------------------------------------⚡️
+  ⚡️|	Running Node Server for ${process.env.NODE_ENV}	|⚡️
+  ⚡️|	    Ready now on port: ${config.port}           |⚡️
+  ⚡️---------------------------------------------------⚡️
   `);
-      })
-  )
-  .catch((error) => console.log(error.message));
-
-export default app;
+    });
+  });
--- test/auth.test.ts
+++ test/auth.test.ts
@@ -1,12 +1,22 @@
-// import { authenticateUser } from '../api/auth'; // assuming the auth file is located in src/auth.ts
-// import { User } from '../src/models'; // assuming User model is imported from src/models.ts
-
+import { Application } from "express";
 import mongoose from "mongoose";
-import request from 'supertest';
-import app from '../src/main';
+import request from "supertest";
+import appPromise from "../src/app";
 
-beforeAll((done) => {
-  done();
+let app: Application;
+let token: string;
+
+beforeAll(async () => {
+  app = await appPromise;
+  const response = await request(app)
+    .post("/api/auth/login")
+    .send({
+      user: {
+        email: "vardiroy4@gmail.com",
+        password: "abc123",
+      },
+    });
+  token = response.body.accessToken;
 });
 
 afterAll((done) => {
@@ -15,8 +25,14 @@ afterAll((done) => {
 });
 
 describe("Authentication", () => {
-  test("GET /", async () => {
-    const res = await request(app).get("/status")
+  test("register", async () => {
+    const res = await request(app)
+      .get("/api/recipes/users")
+      .set("authorization", token);
+    // .send({
+    //   email: "testmail@gmail.com",
+    //   password: "blabla123",
+    // });
     expect(res.statusCode).toEqual(200);
   });
 });
